6abacd1bfebd663224871db7377046b6b7190fc3
LucasBerce71 <lucasberce71@gmail.com>
2022-06-26 20:02:58 -0300

    feat: first commit

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (alignment: .center, spacing: 10) {
+            Text("Olá")
+                .bold()
+                .font(.title)
+                .foregroundColor(.black)
+            Text("Seja Bem Vindo")
+                .lineLimit(nil)
+                .foregroundColor(.black)
+                .font(.body)
+                .multilineTextAlignment(.center)
+            Image("testImage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 300)
+                .clipped()
+                .clipShape(Circle())
+                .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                .overlay(Circle().stroke(Color.red, lineWidth: 2))
+        }.padding(20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro Max")
     }
 }
