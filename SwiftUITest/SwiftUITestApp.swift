fcf56765476842dac1ac3a2023476750b8f7c478
LucasBerce71 <lucasberce71@gmail.com>
2022-06-26 19:29:30 -0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Lucas Jesus on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
